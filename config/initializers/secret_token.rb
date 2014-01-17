e494bb4bfde11cccc28d1cfeab51c93ff331b87c
ColinLLL <colin@originatechina.com>
2014-01-17 17:16:01 +0800

    TestD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestD::Application.config.secret_key_base = 'f5f3fdc636dfe4d63c31b0c97dcd5e788f3def8f8e085c5b71b8f4f2c7a6618887b1d00a63848d3b85ecb2db22d8a102c748ad64c40585b19dbbad1b04ec0246'
